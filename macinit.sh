f031d08b07fec269602afa693c733ed7c6965bd3
sgtn <sgtn@sgtn.local>
2014-07-30 14:59:42 +0900

    add line to brewfile

--- macinit.sh
+++ macinit.sh
@@ -1,3 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install brew-cask
 brew bundle
+sh -c "`curl -fsSL https://raw.github.com/skwp/dotfiles/master/install.sh`"
